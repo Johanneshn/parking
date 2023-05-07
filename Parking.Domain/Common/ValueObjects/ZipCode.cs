8a3c17ed4aa580685d811b0ae7c8d9d58dc818e7
Johannes <jhn@kth.se>
2023-05-07 18:14:06 +0200

    Remove regex attribute from value objects

--- Parking.Domain/Common/ValueObjects/ZipCode.cs
+++ Parking.Domain/Common/ValueObjects/ZipCode.cs
@@ -1,35 +1,27 @@
-﻿namespace Parking.Domain.Common.ValueObjects;
-
-public partial class ZipCode : ValueObject
+﻿public class ZipCode : ValueObject
 {
-    private static readonly Regex Regex = ZipCodeRegex();
+    private static readonly Regex Regex = new(@"^[0-9]{3}\s?[0-9]{2}$", RegexOptions.Compiled | RegexOptions.Singleline);
 
     public ZipCode(string zipCode)
     {
         var value = zipCode.Trim();
 
-        if (!IsValid(value)) throw new ArgumentException($"Zip code '{zipCode}' is in an incorrect format.");
+        if (!IsValid(value))
+        {
+            throw new ArgumentException($"Zip code '{zipCode}' is in an incorrect format.");
+        }
 
         Value = value;
     }
 
     public string Value { get; }
 
-    public override string ToString()
-    {
-        return Value;
-    }
+    public override string ToString() => Value;
 
-    private static bool IsValid(string value)
-    {
-        return Regex.IsMatch(value);
-    }
+    private static bool IsValid(string value) => Regex.IsMatch(value);
 
     protected override IEnumerable<object> GetEqualityComponents()
     {
         yield return Value;
     }
-
-    [GeneratedRegex("^[0-9]{3}\\s?[0-9]{2}$", RegexOptions.Compiled | RegexOptions.Singleline)]
-    private static partial Regex ZipCodeRegex();
 }
--- Parking.Domain/User/ValueObjects/Email.cs
+++ Parking.Domain/User/ValueObjects/Email.cs
@@ -1,35 +1,28 @@
 ﻿namespace Parking.Domain.User.ValueObjects;
 
-public partial class Email : ValueObject
+public class Email : ValueObject
 {
-    private static readonly Regex Regex = EmailValidationRegex();
+    private const string RegexExpression = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+    private static readonly Regex Regex = new(RegexExpression, RegexOptions.Compiled | RegexOptions.Singleline);
 
     public Email(string email)
     {
-        if (!IsValid(email)) throw new ArgumentException(nameof(email));
+        if (!IsValid(email))
+        {
+            throw new ArgumentException(nameof(email));
+        }
 
         Value = email;
     }
 
     public string Value { get; }
 
-    private static bool IsValid(string email)
-    {
-        return Regex.IsMatch(email);
-    }
+    private static bool IsValid(string email) => Regex.IsMatch(email);
 
-    public override string ToString()
-    {
-        return Value;
-    }
+    public override string ToString() => Value;
 
     protected override IEnumerable<object> GetEqualityComponents()
     {
         yield return Value;
     }
-
-    [GeneratedRegex(
-        "^([\\w-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([\\w-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$",
-        RegexOptions.Compiled | RegexOptions.Singleline)]
-    private static partial Regex EmailValidationRegex();
 }
