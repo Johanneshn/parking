06de2aa0d1c850571e1428f6cf31aa7873e51586
Johannes <jhn@kth.se>
2023-05-07 18:38:35 +0200

    Add LongitudeLatitude unit tests

--- Parking.Domain/Common/ValueObjects/LongitudeLatitude.cs
+++ Parking.Domain/Common/ValueObjects/LongitudeLatitude.cs
@@ -4,10 +4,21 @@ public class LongitudeLatitude : ValueObject
 {
     public LongitudeLatitude(double longitude, double latitude)
     {
+        if (IsLongitudeValid(longitude) is false)
+        {
+            throw new ArgumentException("Longitude must be between -180 and 180 degrees.", nameof(longitude));
+        }
+        if (IsLatitudeValid(latitude) is false)
+        {
+            throw new ArgumentException("Latitude must be between -90 and 90 degrees.", nameof(latitude));
+        }
         Longitude = longitude;
         Latitude = latitude;
     }
 
+    private static bool IsLongitudeValid(double value) => value is >= -180 and <= 180;
+    private static bool IsLatitudeValid(double value) => value is >= -90 and <= 90;
+    
     public double Longitude { get; }
     public double Latitude { get; }
 
---
+++ Parking.UnitTests/Domain/Common/ValueObjects/LongitudeLatitudeTests.cs
@@ -0,0 +1,41 @@
+using Parking.Domain.Common.ValueObjects;
+
+namespace Parking.UnitTests.Domain.Common.ValueObjects;
+
+public class LongitudeLatitudeTests
+{
+    [Fact]
+    public void LongitudeLatitude_ShouldCreateValidObject_WhenInputIsValid()
+    {
+        // Arrange
+        const double validLongitude = 45.00;
+        const double validLatitude = 50.00;
+        
+        // Act
+        var sut = new LongitudeLatitude(validLongitude, validLatitude);
+        
+        // Assert
+        sut.Longitude.Should().Be(validLongitude);
+        sut.Latitude.Should().Be(validLatitude);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetInvalidCoordinates))]
+    public void LongitudeLatitude_ShouldThrowInvalidArgumentException_WhenLongitudeIsInvalid(double longitude, double latitude)
+    {
+        // Arrange & Act 
+        Action act = () => new LongitudeLatitude(longitude, latitude);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    public static IEnumerable<object[]> GetInvalidCoordinates =>
+        new List<object[]>
+        {
+            new object[] { -181, 0 },
+            new object[] { 181, 0 },
+            new object[] { 0, -91 },
+            new object[] { 0, 91 },
+        };
+}
